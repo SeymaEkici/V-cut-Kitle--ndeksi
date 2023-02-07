bf84aec0161e5c727d76935eaa91cd5c44925d67
Şeyma Ekici <100785004+SeymaEkici@users.noreply.github.com>
2023-02-07 16:53:12 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double height, weight, index;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+        height = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        weight = input.nextDouble();
+
+        index = weight / (height * height);
+        System.out.println("Vücut Kitle İndeksiniz: " + index);
+    }
+}
